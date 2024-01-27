5a9a5b7d64d3c4a034a2148fcf54d1dce078dea4
Christoph Lahner <clahner92@gmail.com>
2024-01-27 14:55:42 +0100

    Implement goal

--- scenes/main/main.gd
+++ scenes/main/main.gd
@@ -11,18 +11,26 @@ var currentLevel = -1
 @onready var camera : Camera3D = get_node("Camera3D")
 @onready var boundary = get_node("WorldBoundary")
 
+@onready var initialPlayerPosition = player.position
+
+var loosingScene = preload("res://scenes/main/loose.tscn").instantiate()
+
 var initialCameraRotation = deg_to_rad(-70.0)
 var maxRotation = 0.3
-var inputVector = Vector3()
+var inputVector
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	next_level()
 
 func next_level():
+	inputVector = Vector3()
+		
 	if levelNode:
 		remove_child(levelNode)
 		levelNode.queue_free()
+		
+		player.position = initialPlayerPosition
 
 	currentLevel += 1
 	levelNode = levels[currentLevel].instantiate()
@@ -31,7 +39,6 @@ func next_level():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	inputVector = getInputVector()
-
 
 func _physics_process(delta):
 	adjustRotation(delta)
@@ -84,3 +91,6 @@ func _on_sphere_body_entered(body):
 	if body.is_in_group("collectibles"):
 		body.get_parent().remove_child(body)
 		body.queue_free()
+		
+		if body.name == "Goal":
+			next_level()
