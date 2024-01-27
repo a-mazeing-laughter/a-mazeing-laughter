2b0a6cfdd4b6560afbbfa372c121aef4e2b100ce
Bernd Stellwag <burned@zerties.org>
2024-01-27 11:04:39 +0100

    adjust build script

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ rm -rf dist/
 mkdir -p dist
 
 echo 'Building for web ...'
-godot --no-window --export-release "Web" dist/index.html
+godot --headless --export-release "Web" dist/index.html
 echo 'done.'
 
 echo 'Building for linux ...'
-godot --no-window --export-release 'Linux/X11' dist/a-mazeing-laughter.x86_64
+godot --headless --export-release 'Linux/X11' dist/a-mazeing-laughter.x86_64
 chmod a+x dist/a-mazeing-laughter.x86_64
 echo 'done.'
 
 echo 'Building for Windows ...'
-godot --no-window --export-release 'Windows Desktop' dist/a-mazeing-laughter.exe
+godot --headless --export-release 'Windows Desktop' dist/a-mazeing-laughter.exe
 echo 'done.'
