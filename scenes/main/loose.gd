57ef27fc4d1d7d4d168e969b027515bf9c643e01
Thomas Christlieb <thomaschristlieb@hotmail.com>
2024-01-27 11:31:30 +0100

    Center loosing Text

--- scenes/main/loose.gd
+++ scenes/main/loose.gd
@@ -2,8 +2,18 @@ extends Node2D
 
 
 func _ready():
-	pass
+	get_tree().get_root().size_changed.connect(screenSizeChanged) 
+	centerText()
 
 
 func _process(_delta):
 	pass
+
+
+func centerText():
+	var loosingText = get_node("LoosingText")
+	loosingText.position = get_viewport_rect().size/2
+
+
+func screenSizeChanged():
+	centerText()
