cbeeaf579278a23316a66096f043b6292efe0b12
Bernd Stellwag <burned@zerties.org>
2024-01-27 11:15:27 +0100

    fix typo

--- build.sh
+++ build.sh
@@ -6,14 +6,14 @@ rm -rf dist/
 mkdir -p dist
 
 echo 'Building for web ...'
-godot --headless --export-release "Web" dist/index.html
+godot --headless --no-window --export-release "HTML5" dist/index.html
 echo 'done.'
 
 echo 'Building for linux ...'
-godot --headless --export-release 'Linux/X11' dist/a-mazeing-laughter.x86_64
+godot --headless --no-window --export-release 'Linux/X11' dist/a-mazeing-laughter.x86_64
 chmod a+x dist/a-mazeing-laughter.x86_64
 echo 'done.'
 
 echo 'Building for Windows ...'
-godot --headless --export-release 'Windows Desktop' dist/a-mazeing-laughter.exe
+godot --headless --no-window --export-release 'Windows Desktop' dist/a-mazeing-laughter.exe
 echo 'done.'
