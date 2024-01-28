75ec662f5b8bd9f74172e94732e94fb298a889be
Thomas Christlieb <thomaschristlieb@hotmail.com>
2024-01-28 11:10:52 +0100

    Add end screen

--- scenes/main/end.gd
+++ scenes/main/end.gd
@@ -15,7 +15,7 @@ func _process(_delta):
 
 
 func centerText():
-	var loosingText = get_node("LoosingText")
+	var loosingText = get_node("EndText")
 	loosingText.position = get_viewport_rect().size/2
 	loosingText.position.x = loosingText.position.x - loosingText.get_content_width()/2
 	loosingText.position.y = loosingText.position.y - loosingText.get_content_height()/2
